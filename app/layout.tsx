3ba098e4d4fbbd3194518e7d12d830650ac5e8b4
Noam <noam@gradientong.com>
2026-07-10 09:43:03 +0300

    Fix extention bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body>
+      <body suppressHydrationWarning>
         <StickyInvestorBar />
         <LanguageProvider>
           {children}
